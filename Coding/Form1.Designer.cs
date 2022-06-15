959880a6e721c4c34431e7d63af4ce1d305c02a8
xmyron <107299209+xmyron@users.noreply.github.com>
2022-06-15 09:28:28 +0200

    Add files via upload

---
+++ Coding/Form1.Designer.cs
@@ -0,0 +1,253 @@
+﻿namespace Coding
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.сохранитьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.шифрованиеToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.соСмещениемToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.таблицаASCIIToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.дешифрованиеToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.соСмещениемToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.таблицаASCIIToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.настройкиToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.языкToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.русскийToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.українськаToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.englishToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.выйтиToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.Color.Silver;
+            this.menuStrip1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.menuStrip1.GripStyle = System.Windows.Forms.ToolStripGripStyle.Visible;
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem1,
+            this.шифрованиеToolStripMenuItem,
+            this.дешифрованиеToolStripMenuItem,
+            this.настройкиToolStripMenuItem,
+            this.выйтиToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(808, 30);
+            this.menuStrip1.TabIndex = 4;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.сохранитьToolStripMenuItem});
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(56, 26);
+            this.toolStripMenuItem1.Text = "File";
+            // 
+            // сохранитьToolStripMenuItem
+            // 
+            this.сохранитьToolStripMenuItem.Name = "сохранитьToolStripMenuItem";
+            this.сохранитьToolStripMenuItem.Size = new System.Drawing.Size(133, 26);
+            this.сохранитьToolStripMenuItem.Text = "Save";
+            this.сохранитьToolStripMenuItem.Click += new System.EventHandler(this.SaveToolStripMenuItem_Click);
+            // 
+            // шифрованиеToolStripMenuItem
+            // 
+            this.шифрованиеToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.соСмещениемToolStripMenuItem,
+            this.таблицаASCIIToolStripMenuItem});
+            this.шифрованиеToolStripMenuItem.Name = "шифрованиеToolStripMenuItem";
+            this.шифрованиеToolStripMenuItem.Size = new System.Drawing.Size(110, 26);
+            this.шифрованиеToolStripMenuItem.Text = "Encryption";
+            this.шифрованиеToolStripMenuItem.Click += new System.EventHandler(this.DecryptionToolStripMenuItem_Click);
+            // 
+            // соСмещениемToolStripMenuItem
+            // 
+            this.соСмещениемToolStripMenuItem.Name = "соСмещениемToolStripMenuItem";
+            this.соСмещениемToolStripMenuItem.Size = new System.Drawing.Size(193, 26);
+            this.соСмещениемToolStripMenuItem.Text = "With offset";
+            this.соСмещениемToolStripMenuItem.Click += new System.EventHandler(this.OffsetToolStripMenuItem_Click);
+            // 
+            // таблицаASCIIToolStripMenuItem
+            // 
+            this.таблицаASCIIToolStripMenuItem.Name = "таблицаASCIIToolStripMenuItem";
+            this.таблицаASCIIToolStripMenuItem.Size = new System.Drawing.Size(193, 26);
+            this.таблицаASCIIToolStripMenuItem.Text = "Table ASCII";
+            this.таблицаASCIIToolStripMenuItem.Click += new System.EventHandler(this.TableASCIIToolStripMenuItem_Click);
+            // 
+            // дешифрованиеToolStripMenuItem
+            // 
+            this.дешифрованиеToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.соСмещениемToolStripMenuItem1,
+            this.таблицаASCIIToolStripMenuItem1});
+            this.дешифрованиеToolStripMenuItem.Name = "дешифрованиеToolStripMenuItem";
+            this.дешифрованиеToolStripMenuItem.Size = new System.Drawing.Size(112, 26);
+            this.дешифрованиеToolStripMenuItem.Text = "Decryption";
+            this.дешифрованиеToolStripMenuItem.Click += new System.EventHandler(this.DecryptionToolStripMenuItem_Click);
+            // 
+            // соСмещениемToolStripMenuItem1
+            // 
+            this.соСмещениемToolStripMenuItem1.Name = "соСмещениемToolStripMenuItem1";
+            this.соСмещениемToolStripMenuItem1.Size = new System.Drawing.Size(193, 26);
+            this.соСмещениемToolStripMenuItem1.Text = "With offset";
+            this.соСмещениемToolStripMenuItem1.Click += new System.EventHandler(this.OffsetToolStripMenuItem1_Click);
+            // 
+            // таблицаASCIIToolStripMenuItem1
+            // 
+            this.таблицаASCIIToolStripMenuItem1.Name = "таблицаASCIIToolStripMenuItem1";
+            this.таблицаASCIIToolStripMenuItem1.Size = new System.Drawing.Size(193, 26);
+            this.таблицаASCIIToolStripMenuItem1.Text = "Table ASCII";
+            this.таблицаASCIIToolStripMenuItem1.Click += new System.EventHandler(this.TableASCIIToolStripMenuItem1_Click);
+            // 
+            // настройкиToolStripMenuItem
+            // 
+            this.настройкиToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.языкToolStripMenuItem});
+            this.настройкиToolStripMenuItem.Name = "настройкиToolStripMenuItem";
+            this.настройкиToolStripMenuItem.Size = new System.Drawing.Size(86, 26);
+            this.настройкиToolStripMenuItem.Text = "Settings";
+            // 
+            // языкToolStripMenuItem
+            // 
+            this.языкToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.русскийToolStripMenuItem,
+            this.українськаToolStripMenuItem,
+            this.englishToolStripMenuItem});
+            this.языкToolStripMenuItem.Name = "языкToolStripMenuItem";
+            this.языкToolStripMenuItem.Size = new System.Drawing.Size(168, 26);
+            this.языкToolStripMenuItem.Text = "Language";
+            // 
+            // русскийToolStripMenuItem
+            // 
+            this.русскийToolStripMenuItem.Name = "русскийToolStripMenuItem";
+            this.русскийToolStripMenuItem.Size = new System.Drawing.Size(189, 26);
+            this.русскийToolStripMenuItem.Text = "Русский";
+            this.русскийToolStripMenuItem.Click += new System.EventHandler(this.RussianToolStripMenuItem_Click);
+            // 
+            // українськаToolStripMenuItem
+            // 
+            this.українськаToolStripMenuItem.Name = "українськаToolStripMenuItem";
+            this.українськаToolStripMenuItem.Size = new System.Drawing.Size(189, 26);
+            this.українськаToolStripMenuItem.Text = "Українська";
+            this.українськаToolStripMenuItem.Click += new System.EventHandler(this.UkranianToolStripMenuItem_Click);
+            // 
+            // englishToolStripMenuItem
+            // 
+            this.englishToolStripMenuItem.Name = "englishToolStripMenuItem";
+            this.englishToolStripMenuItem.Size = new System.Drawing.Size(189, 26);
+            this.englishToolStripMenuItem.Text = "English";
+            this.englishToolStripMenuItem.Click += new System.EventHandler(this.EnglishToolStripMenuItem_Click);
+            // 
+            // выйтиToolStripMenuItem
+            // 
+            this.выйтиToolStripMenuItem.Name = "выйтиToolStripMenuItem";
+            this.выйтиToolStripMenuItem.Size = new System.Drawing.Size(56, 26);
+            this.выйтиToolStripMenuItem.Text = "Exit";
+            this.выйтиToolStripMenuItem.Click += new System.EventHandler(this.ExitToolStripMenuItem_Click);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.BackColor = System.Drawing.Color.Snow;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.textBox1.Location = new System.Drawing.Point(16, 68);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(775, 344);
+            this.textBox1.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label1.Location = new System.Drawing.Point(16, 416);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 20);
+            this.label1.TabIndex = 6;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.ClientSize = new System.Drawing.Size(808, 454);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Coding";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem сохранитьToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem шифрованиеToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem дешифрованиеToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem соСмещениемToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem таблицаASCIIToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem соСмещениемToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem таблицаASCIIToolStripMenuItem1;
+        public System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ToolStripMenuItem настройкиToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem языкToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem русскийToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem українськаToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem englishToolStripMenuItem;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ToolStripMenuItem выйтиToolStripMenuItem;
+    }
+}
+
---
+++ Coding/Form1.cs
@@ -0,0 +1,218 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Coding
+{
+    public partial class Form1 : Form
+    {
+        string language = "English";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void DecryptionToolStripMenuItem_Click(object sender, EventArgs e) { }
+
+        private void OffsetToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string str = textBox1.Text;
+            textBox1.Text = "";
+            for (int i = 0; i < str.Length; i++)
+            {
+                textBox1.Text += Convert.ToChar(Convert.ToInt32(str[i]) + 2);
+            }
+            if(language == "Russian")
+                label1.Text = "Выполнено шифрование со смещением...";
+            else if (language == "Ukraine")
+                label1.Text = "Виповнено шифрування зі зміщенням...";
+            else if (language == "English")
+                label1.Text = "Encryption with offset...";
+        }
+
+        private void OffsetToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            string str = textBox1.Text;
+            textBox1.Text = "";
+            for (int i = 0; i < str.Length; i++)
+            {
+                textBox1.Text += Convert.ToChar(Convert.ToInt32(str[i]) - 2);
+            }
+            if (language == "Russian")
+                label1.Text = "Выполнено дешифрование со смещением...";
+            else if (language == "Ukraine")
+                label1.Text = "Виповнено дешифрування зі зміщенням...";
+            else if (language == "English")
+                label1.Text = "Decryption with offset...";
+        }
+
+        private void TableASCIIToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string str = textBox1.Text;
+            textBox1.Text = "";
+            foreach (var t in str)
+            {
+                if (Convert.ToInt32(t) > 255)
+                {
+                    textBox1.Text = str;
+                    MessageBox.Show("Character not in a table!");
+                    return;
+                }
+                textBox1.Text += (Convert.ToInt32(t) + 300).ToString();
+            }
+            switch (language)
+            {
+                case "Russian":
+                    label1.Text = "Выполнено шифрование таблицей ASCII...";
+                    break;
+                case "Ukraine":
+                    label1.Text = "Виповнено шифрування таблицею ASCII...";
+                    break;
+                case "English":
+                    label1.Text = "Encryption with table ASCII...";
+                    break;
+            }
+        }
+
+        private void TableASCIIToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            string str = textBox1.Text;
+            textBox1.Text = "";
+            int k = 0;
+            string number = "";
+            for (int i = 0; i < str.Length; i++)
+            {
+                k++;
+                number += str[i];
+                if (k == 3)
+                {
+                    try
+                    {
+                        k = 0;
+                        textBox1.Text += Convert.ToChar(Convert.ToInt32(number) - 300);
+                        number = "";
+                    }
+                    catch (FormatException)
+                    {
+                        textBox1.Text = str;
+                        MessageBox.Show("Invalid text");
+                        return;
+                    }
+                }
+            }
+            if (language == "Russian")
+                label1.Text = "Выполнено дешифрование таблицей ASCII...";
+            else if (language == "Ukraine")
+                label1.Text = "Виповнено дешифрування таблицею ASCII...";
+            else if (language == "English")
+                label1.Text = "Decryption with table ASCII...";
+        }
+
+        private void RussianToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            language = "Russian";
+
+            if (label1.Text == "Виповнено шифрування зі зміщенням..." || label1.Text == "Encryption with offset...")
+                label1.Text = "Выполнено шифрование со смещением...";
+            else if (label1.Text == "Виповнено дешифрування зі зміщенням..." || label1.Text == "Decryption with offset...")
+                label1.Text = "Выполнено дешифрование со смещением...";
+            else if (label1.Text == "Виповнено шифрування таблицею ASCII..." || label1.Text == "Encryption with table ASCII...")
+                label1.Text = "Выполнено шифрование таблицей ASCII...";
+            else if (label1.Text == "Виповнено дешифрування таблицею ASCII..." || label1.Text == "Decryption with table ASCII...")
+                label1.Text = "Выполнено дешифрование таблицей ASCII...";
+
+            toolStripMenuItem1.Text = "Файл";
+            сохранитьToolStripMenuItem.Text = "Сохранить";
+
+            шифрованиеToolStripMenuItem.Text = "Шифрование";
+            соСмещениемToolStripMenuItem.Text = "Со смещением";
+            таблицаASCIIToolStripMenuItem.Text = "Таблица ASCII";
+
+            дешифрованиеToolStripMenuItem.Text = "Дешифрование";
+            соСмещениемToolStripMenuItem1.Text = "Со смещением";
+            таблицаASCIIToolStripMenuItem1.Text = "Таблица ASCII";
+
+            настройкиToolStripMenuItem.Text = "Настройки";
+            языкToolStripMenuItem.Text = "Язык";
+            выйтиToolStripMenuItem.Text = "Выход";
+        }
+
+        private void UkranianToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            language = "Ukraine";
+
+            if (label1.Text == "Выполнено шифрование со смещением..." || label1.Text == "Encryption with offset...")
+                label1.Text = "Виповнено шифрування зі зміщенням...";
+            else if (label1.Text == "Выполнено дешифрование со смещением..." || label1.Text == "Decryption with offset...")
+                label1.Text = "Виповнено дешифрування зі зміщенням...";
+            else if (label1.Text == "Выполнено шифрование таблицей ASCII..." || label1.Text == "Encryption with table ASCII...")
+                label1.Text = "Виповнено шифрування таблицею ASCII...";
+            else if (label1.Text == "Выполнено дешифрование таблицей ASCII..." || label1.Text == "Decryption with table ASCII...")
+                label1.Text = "Виповнено дешифрування таблицею ASCII...";
+
+            toolStripMenuItem1.Text = "Файл";
+            сохранитьToolStripMenuItem.Text = "Зберегти";
+
+            шифрованиеToolStripMenuItem.Text = "Шифрування";
+            соСмещениемToolStripMenuItem.Text = "Зі зміщенням";
+            таблицаASCIIToolStripMenuItem.Text = "Таблиця ASCII";
+
+            дешифрованиеToolStripMenuItem.Text = "Дешифрування";
+            соСмещениемToolStripMenuItem1.Text = "Зі зміщенням";
+            таблицаASCIIToolStripMenuItem1.Text = "Таблиця ASCII";
+
+            настройкиToolStripMenuItem.Text = "Налаштування";
+            языкToolStripMenuItem.Text = "Мова";
+            выйтиToolStripMenuItem.Text = "Вихід";
+        }
+
+        private void EnglishToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            language = "English";
+
+            if (label1.Text == "Выполнено шифрование со смещением..." || label1.Text == "Виповнено шифрування зі зміщенням...")
+                label1.Text = "Encryption with offset...";
+            else if (label1.Text == "Выполнено дешифрование со смещением..." || label1.Text == "Виповнено дешифрування зі зміщенням...")
+                label1.Text = "Decryption with offset...";
+            else if (label1.Text == "Выполнено шифрование таблицей ASCII..." || label1.Text == "Виповнено шифрування таблицею ASCII...")
+                label1.Text = "Encryption with table ASCII...";
+            else if (label1.Text == "Выполнено дешифрование таблицей ASCII..." || label1.Text == "Виповнено дешифрування таблицею ASCII...")
+                label1.Text = "Decryption with table ASCII...";
+
+            toolStripMenuItem1.Text = "File";
+            сохранитьToolStripMenuItem.Text = "Save";
+
+            шифрованиеToolStripMenuItem.Text = "Encryption";
+            соСмещениемToolStripMenuItem.Text = "With offset";
+            таблицаASCIIToolStripMenuItem.Text = "Table ASCII";
+
+            дешифрованиеToolStripMenuItem.Text = "Decryption";
+            соСмещениемToolStripMenuItem1.Text = "With offset";
+            таблицаASCIIToolStripMenuItem1.Text = "Table ASCII";
+
+            настройкиToolStripMenuItem.Text = "Settings";
+            языкToolStripMenuItem.Text = "Language";
+            выйтиToolStripMenuItem.Text = "Exit";
+        }
+
+        private void SaveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            File.WriteAllText("Output.txt", textBox1.Text);
+        }
+
+        private void label1_Click(object sender, EventArgs e) { }
+
+        private void ExitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
